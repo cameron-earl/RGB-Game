9fdcb182e0927732d051e27eff48dfb262782926
randomraccoon <randomraccoon@gmail.com>
2017-06-10 11:33:41 -0700

    add keypress for new game n or space work also adjusted shadow for h1

--- rgbGame.js
+++ rgbGame.js
@@ -155,6 +155,12 @@ function createEventListeners() {
         }
     });
 
+    document.addEventListener("keypress", function(ev) {
+        if (ev.key === " " || ev.key === "n") {
+            newGame();
+        }
+    });
+
     document.addEventListener("keyup", function(ev) {
         if (!ev.shiftKey && shiftDown) {
             shiftDown = false;
